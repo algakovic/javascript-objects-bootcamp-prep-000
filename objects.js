4bb03b08aa608bda452010e747a6f1dd98c42ba6
Aleksandar Gakovic <algakovic@gmail.com>
2018-07-10 04:47:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {A7X: " A little piece of Heaven"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = value
+  playlist[artistName] = songTitle
   return playlist
 }
